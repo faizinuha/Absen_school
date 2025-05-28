9af19dce9ec92c38ea542d8dbdf2624121ffda58
TZ <rozakadm@gmail.com>
2025-05-28 18:17:30 +0700

    Bugs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,9 +31,9 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="Login" />
+        {/* <Stack.Screen name="Login" />
         <Stack.Screen name="register" />
-        <Stack.Screen name="forgot-password" />
+        <Stack.Screen name="forgot-password" /> */}
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
